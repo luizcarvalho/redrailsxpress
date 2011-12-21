b87fd3c6db7cc3fa1f2550a42291c6f90f9630f9
Luiz Carvalho <maximusmano@gmail.com>
2011-12-21 10:37:03 -0300

    removendo pasta de adm

---
+++ administracao/upgrade-functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * WordPress Upgrade Functions. Old file, must not be used. Include
+ * administracao/includes/upgrade.php instead.
+ *
+ * @deprecated 2.5
+ * @package WordPress
+ * @subpackage Administration
+ */
+
+_deprecated_file( basename(__FILE__), '2.5', 'administracao/includes/upgrade.php' );
+require_once(ABSPATH . 'administracao/includes/upgrade.php');
+?>
